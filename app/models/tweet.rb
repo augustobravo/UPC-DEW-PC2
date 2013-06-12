7659a499df68b02aa716ff4c61a2008b5b304016
root <eoliveros@softlandperu.com>
2013-06-11 22:39:29 -0500

    PC02

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,10 +2,10 @@ class Tweet < ActiveRecord::Base
   attr_accessible :message, :url, :user
 
   Twitter.configure do |config|
-    config.consumer_key = '3BZ2Fk5yzOXN0QSPRZqiA'
-    config.consumer_secret = '1AGkWl2MKX73K5Muop7NMwd5xhUyZkchWzEgAQieAwE'
-    config.oauth_token = '344334932-0WFeQD28tyl2NNP45oRg5mIG7X0hPQZvB9hZffZz'
-    config.oauth_token_secret = '2dlCgTmslZvQMDmqobi0tITaJ9mtJBamgKrTudCKM'
+    config.consumer_key = 'h18mqPUlfvgJe59qXu4Mw'
+    config.consumer_secret = 'KbSdDaarKmPMROPdVBWHVrCfPoPUWYUBP6uwpLmZE'
+    config.oauth_token = '71257300-8UtI4QdrmYCsI6CyfLLkXwz3VymemE4SU8g91tjOD'
+    config.oauth_token_secret = 'BqGdU4aVyPwS8ajvL6DyagqegxzuBmdxztXYS9YxqE'
   end
   def read_message
     return "no hay user" if self.user.blank?
